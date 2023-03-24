a308b55577ec583f5a57889026feffc6b71b0bfd
Medill-East <newmatyjamariusz@gmail.com>
2023-03-24 19:27:06 +0100

    Added Timer and implemented performing permutations of experiment settings, time intervals and trial numbers

--- Assets/Experiment.cs
+++ Assets/Experiment.cs
@@ -3,188 +3,309 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
 public class Experiment : MonoBehaviour {
-    List<ExperimentSetting> settings;
+    private List<ExperimentSetting> settings;
     CircularArray array;
     public GameObject canvas;
+
     bool isStarted = false;
+    List<string> lettersD;
+    List<string> lettersT;
+    int trialAmount;
+    bool isFinished = false;
+
+
+    ExperimentSetting currentSetting;
+    float currentTimeInterval;
+    int trialsSoFar = 0;
+    TimeCounter timer = new TimeCounter();
 
     private GameObject fixCross;
 
-    public Experiment() 
+
+    int currentSettingNumber;
+    int currentTimeIntervalNumber;
+    bool isCurrentTrialFinished = true;
+
+
+    public Experiment()
     {
-        Debug.Log("Contructor called");
-        int numbOfSettings = 3;
-        int trialAmount = 50;
-        List<double> times = new List<double>(){5.0, 10.0, 20.0, 50.0};
-        ExperimentSetting setting1 = new ExperimentSetting(8, 2, false, false);
-        ExperimentSetting setting2 = new ExperimentSetting(5, 5, false, false);
-        ExperimentSetting setting3 = new ExperimentSetting(2, 8, false, false);
-        //array = new CircularArray();
-        //canvas = GameObject.Find("MainCanvas");
-        //array.myObj.transform.SetParent(canvas.transform);
-        
-
-        List<ExperimentSetting> settings = new List<ExperimentSetting>(){setting1, setting2, setting3};
-        this.settings = settings;
+        trialAmount = 2;
+        lettersT = new List<string>() { "a", "b", "c" };
+        lettersD = new List<string>() { "o" };
+        ExperimentSetting setting1 = new ExperimentSetting(4, 4, false, false, 1);
+        settings = new List<ExperimentSetting>() { setting1 };
+        currentSetting = settings[currentSettingNumber];
+        currentTimeInterval = currentSetting.timeIntervals[currentTimeIntervalNumber];
     }
 
-    /*public Execute() {
-        foreach (ExperiementSetting setting in this.settings) {
-            for (int i = 0; i < times.Count; i++) {
-                //Populate canvas for times[i] ms
-                //Wait for 'ready' check again, to populate new experiment
-            }
-        }
-        //Write configurations to some object
-    }
-
-    public Save(string configurations) {
-        //Write configurations to .csv file
-    }
-
-    public Populate() {
-        
-    }*/
- 
- 
     // Start is called before the first frame update
     void Start()
     {
         fixCross = GameObject.Find("FixationCross");
-
         array = new CircularArray();
-        //centerPos = canvas.transform.position;
-        array.Init(3,10);
-        //array.myObj.transform.SetParent(canvas.transform);
+        array.Init(3, 10);
         array.myObj.transform.SetParent(fixCross.transform);
-        array.myObj.transform.position = new Vector3(0,0,0); //canvas.transform.position;
-
-        int numbOfObjects = 10; 
-        List<GameObject> objects = new List<GameObject>();
-
-        for(int i = 0; i <numbOfObjects; i++)
-        {
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            objects.Add(sphere);
-        }
-        
-        array.PutIntoSlots(objects);
-
+        array.myObj.transform.position = fixCross.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("canvas pos: " + canvas.transform.position);
-        /*
-        if (isReady) {
-            Experiment exp = new Experiment()
-            exp.Execute()
-            exp.Save()
-            exp.Delete()
+        if(!isFinished)
+        {
+            timer.Update();
+            if (!timer.isRunning && isCurrentTrialFinished)
+            {
+                Debug.Log("setting " + currentSettingNumber + " time: " + currentSetting.timeIntervals[currentTimeIntervalNumber] + " trial " + trialsSoFar);
+                var ret = GenerateSymbols(currentSetting.numbOfTargets, currentSetting.numbOfDistractors, lettersT, lettersD);
+                //Debug.Log("Symbols Generated");
+                array.PutIntoSlots(ret.Item1, ret.Item2);
+                //Debug.Log("Symbols in Slots");
+                if (currentSetting.targetsFarAway) { array.PushBack(currentSetting.depth, true); }
+                else if (currentSetting.distractorsFarAway) { array.PushBack(currentSetting.depth, false); }
+                //Debug.Log("Symbols Pushed");
+                timer.StartCounting(currentTimeInterval);
+                //Debug.Log("Timer Started");
+                isCurrentTrialFinished = false;
+            }
+            else if (!timer.isRunning)
+            {
+                isCurrentTrialFinished = true;
+                //Debug.Log("time diff timer:" + (Time.time - timer.startTime));
+                Debug.Log("estimated time diff:" + (Time.time - timer.startTime + Time.deltaTime));
+                array.Clear();
+                //Debug.Log("Cleared");
+                TryToUpdateSetting();
+
+            }
+
         }
-        */
-
-        // if (!isStarted && OVRInput.GetDown(OVRInput.Button.One)) {
-        //     isStarted = true;
-        // }
-        
-        // //Debug.Log(array.ToString());
-
-        // array.Init(3,4);
-
-        // int numbOfObjects = 4; 
-        // List<GameObject> objects = new List<GameObject>();
-
-        // for(int i = 0; i <numbOfObjects; i++)
-        // {
-        //     GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        //     objects.Add(sphere);
-        // }
-        
-        // array.PutIntoSlots(objects);
-
     }
-}
 
-public class ExperimentSetting {
-    int targets;
-    int distractors;
-    bool targetsFarAway; 
-    bool distractorsFarAway;
-    
-    public ExperimentSetting(int targets, int distractors, bool targetsFarAway,bool distractorsFarAway) {
-        this.targets = targets;
-        this.distractors = distractors;
-        this.targetsFarAway = targetsFarAway;
-        this.distractorsFarAway = distractorsFarAway;
+
+    public (List<GameObject>, List<GameObject>) GenerateSymbols(int numbOfTargets, int numbOfDistractors, List<string> lettersT, List<string> lettersD)
+    {
+        List<GameObject> targets = new List<GameObject>();
+        List<GameObject> distractors = new List<GameObject>();
+        System.Random rnd = new System.Random();
+
+
+        for (int i = 0; i < numbOfTargets; i++)
+        {
+            int c = rnd.Next(0, lettersT.Count);
+            GameObject newTarget = Instantiate(GameObject.Find(lettersT[c]));
+            targets.Add(newTarget);
+        }
+        for (int i = 0; i < numbOfDistractors; i++)
+        {
+            int c = rnd.Next(0, lettersD.Count);
+            GameObject newDistractor = Instantiate(GameObject.Find(lettersD[c])); 
+            distractors.Add(newDistractor);
+        }
+        return (targets, distractors);
     }
+
+
+    public void TryToUpdateSetting()
+    {
+        if(trialsSoFar == trialAmount) 
+        {
+            if(currentTimeIntervalNumber != currentSetting.timeIntervals.Count-1)
+            {
+                currentTimeIntervalNumber += 1;
+                currentTimeInterval = currentSetting.timeIntervals[currentTimeIntervalNumber];
+                
+                
+            }
+            else
+            {
+                if(currentSettingNumber != settings.Count -1)
+                {
+                    currentSettingNumber += 1;
+                    currentSetting = settings[currentSettingNumber];
+                }
+
+                else
+                {
+                    isFinished = true;
+                }
+            }
+
+            trialsSoFar = 0;
+        }
+        trialsSoFar += 1;
+    }
+
 }
+    public class ExperimentSetting {
+        public int numbOfTargets { private set; get; }
+        public int numbOfDistractors { private set; get; }
+        public bool targetsFarAway { private set; get; }
+        public bool distractorsFarAway { private set; get; }
+        public int depth { private set; get; }
+        public List<float> timeIntervals { private set; get; } = new List<float>() { 2.0f, 5.0f, 10.0f };
 
-/*public class Timing {
-    // double start_time = Time.time +deltaTime
-    // curr_time = time.getTotalTime()
-    // if((curr_time - (start_time + deltatime)))
-    // {
-    //IsTimeUp(){};
-    //stopShowingSymbols();
+        public ExperimentSetting(int NumbOftargets, int NumbOfdistractors, bool targetsFarAway, bool distractorsFarAway, int Depth, List<float> TimeIntervals) {
+            this.numbOfTargets = NumbOftargets;
+            this.numbOfDistractors = NumbOfdistractors;
+            this.targetsFarAway = targetsFarAway;
+            this.distractorsFarAway = distractorsFarAway;
+            this.depth = Depth;
+        }
 
-    //Timing.Run();
-    //if(Timing.isTimeUp()){        }
-    /*if (OVRInput.GetDown(OVRInput.Button.One)) {
-        rog.Populate(centerObject);
-    }*/
+        public ExperimentSetting(int NumbOftargets, int NumbOfdistractors, bool targetsFarAway, bool distractorsFarAway, int Depth)
+        {
+            this.numbOfTargets = NumbOftargets;
+            this.numbOfDistractors = NumbOfdistractors;
+            this.targetsFarAway = targetsFarAway;
+            this.distractorsFarAway = distractorsFarAway;
+            this.depth = Depth;
+        }
+    }
 
 
-public class CircularArray
+
+public class CircularArray : MonoBehaviour
 {
     public GameObject myObj;
     private Vector3[] slots;
     private int numbOfSlots;
-    private List<GameObject> symbols;
+    private List<GameObject> targets;
+    private List<GameObject> distractors;
 
     public CircularArray() {
     }
 
-    public void Init(float radius, int NumbOfSlots){
-        myObj = new GameObject(); 
+    public void Init(float radius, int NumbOfSlots)
+    {
+        myObj = new GameObject();
         numbOfSlots = NumbOfSlots;
 
         slots = new Vector3[NumbOfSlots];
         Debug.Log(slots.Length);
-        float angleStep = 360/NumbOfSlots;
+        float angleStep = 360 / NumbOfSlots;
         float currAngle = 0f;
-        //Math.PI / 180 *
-        //Math.PI / 180
-        for (int i = 0; i < NumbOfSlots; i++){
-            float x = (float) (radius * Math.Cos( currAngle)); 
-            float y = (float) (radius * Math.Sin(  currAngle)); 
-            slots[i] = new Vector3(x,y,0.0f);
+        for (int i = 0; i < NumbOfSlots; i++)
+        {
+            float x = (float)(radius * Math.Cos(currAngle));
+            float y = (float)(radius * Math.Sin(currAngle));
+            slots[i] = new Vector3(x, y, 0.0f);
             currAngle += angleStep;
-            //slots[i] = new Vector3(1,1,0.0f);
-            Debug.Log("slot "+i+" = "+"("+x+";"+y+")");
+            Debug.Log("slot " + i + " = " + "(" + x + ";" + y + ")");
+        }
+
+    }
+
+    public void PutIntoSlots(List<GameObject> Targets, List<GameObject> Distractors)
+    {
+        List<int> found = new List<int>();
+        distractors = Distractors;
+        targets = Targets;
+        System.Random rnd = new System.Random();
+        int c = rnd.Next(0, numbOfSlots);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            targets[i].transform.SetParent(myObj.transform);
+           
+            while (found.Contains(c))
+            { c = rnd.Next(0, numbOfSlots); }
+                
+            found.Add(c);
+            targets[i].transform.position = myObj.transform.position +  slots[c];
+
+        }
+        for (int i = 0; i < distractors.Count; i++)
+        {
+            distractors[i].transform.SetParent(myObj.transform);
+            while (found.Contains(c))
+            { c = rnd.Next(0, numbOfSlots); }
+            found.Add(c);
+            distractors[i].transform.position = myObj.transform.position + slots[c];
+
+
+        }
+
+
+    }
+
+    public void PushBack(int depth, bool pushTargets)
+    {
+        Vector3 v = new Vector3(0, 0, depth);
+        if (pushTargets)
+        {
+            for (int i = 0; i < targets.Count; i++)
+            {
+                targets[i].transform.position -= v;
+                
+            }
+        }
+        else
+        {
+            for (int i = 0; i < distractors.Count; i++)
+            {
+                distractors[i].transform.position -= v;
+            }
+        }
+        
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < targets.Count; i++)
+        {
+            Destroy(targets[i]);
+
+        }
+        for (int i = 0; i < distractors.Count; i++)
+        {
+            Destroy(distractors[i]);
         }
     }
 
-    public void PutIntoSlots(List<GameObject> objects)
+}
+
+public class TimeCounter: MonoBehaviour
+{
+    float secondsLeft;
+    public bool isRunning { private set; get; } = false;
+    bool isFirstFrame = false;
+
+    public float startTime;
+    
+    public void StartCounting(float secondsToWait)
     {
-        List<int> found = new List<int>(); 
-        symbols = objects;
-        // for (int i = 0; i < objects.Count; i++) {
-        //     objects[i].transform.SetParent(myObj.transform);
-        //     System.Random rnd = new System.Random();
-        //     int c = rnd.Next(0,numbOfSlots);
-        //     if(!found.Contains(c))
-        //     {
-        //         found.Add(c);
-        //         objects[i].transform.position = slots[c];
-                
-        //     }
-        // }
-        for (int i = 0; i < objects.Count; i++) {
-            objects[i].transform.SetParent(myObj.transform);
-            objects[i].transform.position = slots[i];
+        secondsLeft = secondsToWait;
+        isRunning = true;
+        isFirstFrame = true;
+    }
+    void Start()
+    {
+    }
+
+    public void Update()
+    {
+        if (isFirstFrame)
+        {
+            isFirstFrame = false;
+            startTime = Time.time;
+            return;
+        }
+
+        if(isRunning)
+        {
+            
+            secondsLeft -= Time.deltaTime;
+        }
+        if( isRunning &&  (secondsLeft - Time.deltaTime)  < 0)
+        {
+            isRunning = false;
+           
         }
     }
+
 }
+
+
+
